bb5e337379440bcd14da847acb1eab860a809f4a
Abhishek singh chauhan <avi7000201383@gmail.com>
2024-02-07 13:47:38 +0530

    added clip paths

--- src/components/NamasteDevTestimonials.jsx
+++ src/components/NamasteDevTestimonials.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ClipPathOne from "./clipPaths/ClipPathOne";
+import ClipPathTwo from "./clipPaths/ClipPathTwo";
 
 const featuredTestimonial = {
   body: "Special thanks to Akshay Saini for the 'Namaste React' course, his teaching style, particularly in the explanation of challenging topics like Redux, lifecycle methods and context API, was outstanding. His ability to simplify these intricate topics while maintaining enthusiasm made the learning journey both enlightening and enjoyable.",
@@ -161,30 +163,9 @@ function classNames(...classes) {
 const NamasteDevTestimonials = () => {
   return (
     <div className="relative isolate bg-neutral-900 pb-32 pt-24 sm:pt-32">
-      <div
-        className="absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl"
-        aria-hidden="true"
-      >
-        <div
-          className="ml-[max(50%,38rem)] aspect-[1313/771] w-[82.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div>
-      <div
-        className="absolute inset-x-0 top-0 -z-10 flex transform-gpu overflow-hidden pt-32 opacity-25 blur-3xl sm:pt-40 xl:justify-end"
-        aria-hidden="true"
-      >
-        <div
-          className="ml-[-22rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] xl:ml-0 xl:mr-[calc(50%-12rem)]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div>
+      <ClipPathOne />
+
+      <ClipPathTwo />
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-xl text-center">
           <h2 className="text-xl font-semibold leading-8 tracking-tight text-amber-400">
